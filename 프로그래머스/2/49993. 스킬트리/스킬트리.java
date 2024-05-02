d02b14738259b981ee2e35c716b8e4af5c0333c0
GyungKu <130152696+GyungKu@users.noreply.github.com>
2024-05-02 22:51:10 +0900

    [level 2] Title: 스킬트리, Time: 0.03 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -4,14 +4,10 @@ class Solution {
 
         for (int i = 0; i < skill_trees.length; i++) {
             boolean flag = true;
-            int idx = skill_trees[i].indexOf(skill.charAt(0));
-            for (int j = 1; j < skill.length(); j++) {
+            int idx = Integer.MIN_VALUE;
+            for (int j = 0; j < skill.length(); j++) {
                 int tmp = skill_trees[i].indexOf(skill.charAt(j));
-                if (idx == -1 && tmp > -1) {
-                    flag = false;
-                    break;
-                }
-                if (tmp != -1 && idx > tmp) {
+                if ((idx == -1 && tmp > -1) || (tmp != -1 && idx > tmp)) {
                     flag = false;
                     break;
                 }
