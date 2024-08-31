d64ba7a738257a09d381ec1abd996e22c3206d86
GyungKu <130152696+GyungKu@users.noreply.github.com>
2024-08-31 11:41:54 +0900

    [Silver II] Title: 촌수계산, Time: 168 ms, Memory: 17796 KB -BaekjoonHub

---
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -0,0 +1,53 @@
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+
+    static int n, x, y, m;
+    static int[][] arr;
+    static boolean[] visited;
+    static int[] result;
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        x = sc.nextInt();
+        y = sc.nextInt();
+        m = sc.nextInt();
+
+        arr = new int[n+1][n+1];
+        visited = new boolean[n+1];
+        result = new int[n+1];
+
+        for (int i = 0; i < m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            arr[a][b] = 1;
+            arr[b][a] = 1;
+        }
+
+        visited[x] = true;
+        result[y] = -1;
+        bfs();
+        System.out.println(result[y]);
+
+    }
+
+
+    static void bfs() {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(x);
+
+        while (!queue.isEmpty()) {
+            Integer cur = queue.poll();
+            for (int i = 1; i <= n; i++) {
+                if (!visited[i] && arr[cur][i] == 1) {
+                    visited[i] = true;
+                    result[i] = result[cur] + 1;
+                    queue.offer(i);
+                }
+            }
+        }
+    }
+
+}
